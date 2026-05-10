1c87a96ab442ecac14aff50f65caf00e57635ed0
wernes-web <wernesvanwyk@gmail.com>
2026-05-10 12:24:02 +0200

    Adding postgres

--- app/main.py
+++ app/main.py
@@ -1,9 +1,32 @@
 import paho.mqtt.client as mqtt
 import json
+import psycopg2
+
+
 
 
 BROKER = "192.168.1.17"
 TOPIC = "sensor/moisture"
+
+conn = psycopg2.connect(
+    host="postgres",
+    database="sensordb",
+    user="postgres",
+    password="password123"
+)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS moisture_data (
+    id SERIAL PRIMARY KEY,
+    timestamp TEXT,
+    moisture INTEGER
+)
+""")
+
+conn.commit()
+
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT broker")
@@ -17,6 +40,14 @@ def on_message(client, userdata, msg):
 
         moisture = data.get("analogValue")
         timestamp = data.get("timestamp")
+
+        cursor.execute(
+            "INSERT INTO moisture_data (timestamp, moisture) VALUES (%s, %s)",
+            (timestamp, moisture)
+        )
+
+        conn.commit()
+        print("DB insert done", flush=True)
 
         print("----- Sensor Data  -----", flush=True)
         print(f"Topic: {msg.topic}", flush=True)
